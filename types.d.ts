1f906dddeab3160d3bf74be01f75713e09af298e
mikihockey3-jpg <miki.hockey3@gmail.com>
2026-02-03 18:47:27 +0100

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,12 @@
-export {};
+export interface Player {
+  id: number;           // ID unico del giocatore
+  name: string;         // Nome del giocatore
+  team: string;         // Nome della squadra
+  position: string;     // Ruolo (es: attaccante, difensore, portiere)
+  photo?: string;       // URL della foto (opzionale)
+  stats?: Record<string, number>; // Statistiche opzionali, es: gol, assist
+}
+
+export interface PluginSettings {
+  sample?: string; // Puoi aggiungere qui altre impostazioni che vuoi far configurare
+}
